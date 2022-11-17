33c11b9ffbf0e114ede46386a4f9baffb057167e
AmmonArctech <115682702+AmmonArctech@users.noreply.github.com>
2022-11-16 19:14:00 -0700

    Pushed up the folders

---
+++ cmake-build-schizo-debug/CMakeFiles/Project_1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/a/agleaso4/CLionRemote/Project1/Datalog.cpp" "/users/guest/a/agleaso4/CLionRemote/Project1/cmake-build-schizo-debug/CMakeFiles/Project_1.dir/Datalog.cpp.o"
+  "/users/guest/a/agleaso4/CLionRemote/Project1/Lexer.cpp" "/users/guest/a/agleaso4/CLionRemote/Project1/cmake-build-schizo-debug/CMakeFiles/Project_1.dir/Lexer.cpp.o"
+  "/users/guest/a/agleaso4/CLionRemote/Project1/main.cpp" "/users/guest/a/agleaso4/CLionRemote/Project1/cmake-build-schizo-debug/CMakeFiles/Project_1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././Lab1PassOffCases/1-70,"
+  "../Lab1PassOffCases/1-90,"
+  "../Lab1PassOffCases/1-100,"
+  "../Lab2PassOffCases/2-80,"
+  "../Lab2PassOffCases/2-100"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
